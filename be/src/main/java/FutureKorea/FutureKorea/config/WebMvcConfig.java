2fa755dbf5724c68f824e86f861c84558473a284
Going9 <ickdor123@gmail.com>
2023-11-28 17:54:53 +0900

    Feats: OAuth

---
+++ be/src/main/java/FutureKorea/FutureKorea/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package FutureKorea.FutureKorea.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableScheduling
+@EnableTransactionManagement
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://localhost:3000")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
