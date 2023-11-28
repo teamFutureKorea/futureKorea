f7a5e2d6780ebd224853423a16dc1214b3f8423c
장성운 <sungoon0646@gmail.com>
2023-11-28 09:59:31 +0900

    init : 프로젝트 생성

---
+++ be/src/main/java/FutureKorea/FutureKorea/FutureKoreaApplication.java
@@ -0,0 +1,13 @@
+package FutureKorea.FutureKorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureKoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureKoreaApplication.class, args);
+	}
+
+}
