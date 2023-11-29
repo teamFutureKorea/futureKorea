73de46ba19b191c51fd27a5668e7bffde4c58267
SeungHyunH <vkflek1232@naver.com>
2023-11-29 11:14:19 +0900

    Feat : CORS 해결

--- be/src/main/java/FutureKorea/FutureKorea/config/WebMvcConfig.java
+++ be/src/main/java/FutureKorea/FutureKorea/config/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://localhost:3000")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
 }
